c51bc128ff3b384ab110c45a5307aa1e06a097a2
Ademir C. Gabardo <ademir.gabbardo@gmail.com>
2017-08-20 09:58:18 +1000

    Lesson 53 creating a service

---
+++ src/services/quotes.ts
@@ -0,0 +1,21 @@
+import { Quote} from "../data/quote.interface";
+
+export class QuotesService{
+  private favouriteQuotes: Quote[] = [];
+
+  add(quote:Quote){
+    this.favouriteQuotes.push(quote);
+  }
+
+  remove(quote:Quote){
+    const position = this.favouriteQuotes.findIndex((quoteEl:Quote)=>{
+      return quoteEl.id == quote.id;
+    });
+    this.favouriteQuotes.splice(position,1);
+  }
+
+  get(){
+    return this.favouriteQuotes.slice();
+  }
+
+}
